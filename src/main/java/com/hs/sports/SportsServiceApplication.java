4a906373b5659c280579dc4591b85ca4ae5e8e41
HarshaHS <harsha.h.sadashiva@gmail.com>
2016-12-22 10:58:34 -0700

    Initial code commit

---
+++ src/main/java/com/hs/sports/SportsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.hs.sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsServiceApplication.class, args);
+	}
+}
